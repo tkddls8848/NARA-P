a857cd1c79b0b682a0a3d0ef4a4b11879adcfe40
PSI <tkddls8848@gmail.com>
2022-02-05 21:44:12 +0900

    [backend] sort data by date && data remodel

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,12 @@ const app = express()
 const dotenv = require('dotenv').config({ path: '../.env' })
 const cors = require('cors')
 const moment = require('moment')
-
+/*
+각 용역, 물품, 외자 별 numOfRows:20, totalCount가 존재하여 많은 수의 건수 검색을하면 20개의 items 만 나옴
+*/
 const today = moment().format('YYYYMMDD0000')
 const yesterday = moment().subtract(7, 'days').format('YYYYMMDD0000')
+
 const serviceKey = process.env.SERVICE_KEY
 const datasize = 'numOfRows=20&pageNo=1'
 const date = 'inqryBgnDt='+ yesterday +'&inqryEndDt=' + today
@@ -18,37 +21,6 @@ app.use(express.urlencoded( {extended : true } ))
 app.listen(process.env.PORT, () => {
     console.log("SERVER ON")
     console.log("TODAY", today, yesterday, typeof(yesterday))
-})
-
-app.get('/task/sajeon/:departname', (req, res) => {
-    const url = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoThng'
-    const url1 = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoCnstwk'
-    const url2 = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoServc'
-    const departname = 'rlDminsttNm=' +encodeURIComponent(req.params.departname)
-    console.log(departname)
-    const api = (url + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
-    const api1 = (url1 + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
-    const api2 = (url2 + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
-    let dataSet = []
-
-    getData = async (api) => {
-        const array = api.split('||')       
-        await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
-        .then(axios.spread((result, result1, result2) => {
-            if (result.data.response.body.totalCount != 0) {
-                dataSet.push(result.data.response.body)
-            }
-            if (result1.data.response.body.totalCount != 0) {
-                dataSet.push(result1.data.response.body)
-            }
-            if (result2.data.response.body.totalCount != 0) {
-                dataSet.push(result2.data.response.body)
-            }
-            console.log("dataSet", dataSet)
-            res.json({'sajeon' : dataSet})
-       }))
-    }
-    getData(api + '||' + api1 + '||' + api2)
 })
 
 //기본 부서 : 국민건강보험공단
@@ -68,15 +40,63 @@ app.get('/task/sajeon', (req, res) => {
         await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
         .then(axios.spread((result, result1, result2) => {
             if (result.data.response.body.totalCount != 0) {
-                dataSet.push(result.data.response.body)
+                for(let i = 0 ; i < result.data.response.body.items.length ; i++){
+                    dataSet.push(result.data.response.body.items[i])
+                }                
             }
             if (result1.data.response.body.totalCount != 0) {
-                dataSet.push(result1.data.response.body)
+                for(let i = 0 ; i < result1.data.response.body.items.length ; i++){
+                    dataSet.push(result1.data.response.body.items[i])
+                }                
             }
             if (result2.data.response.body.totalCount != 0) {
-                dataSet.push(result2.data.response.body)
+                for(let i = 0 ; i < result2.data.response.body.items.length ; i++){
+                    dataSet.push(result2.data.response.body.items[i])
+                }                
             }
-            res.json({'sajeon' : dataSet})
+            dataSet.sort((a, b) => {
+                return a.rcptDt > b.rcptDt ? -1 :  a.rcptDt < b.rcptDt ? 1 : 0
+            })
+            res.send(dataSet)
+       }))
+    }
+    getData(api + '||' + api1 + '||' + api2)
+})
+
+app.get('/task/sajeon/:departname', (req, res) => {
+    const url = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoThng'
+    const url1 = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoCnstwk'
+    const url2 = 'http://apis.data.go.kr/1230000/HrcspSsstndrdInfoService/getInsttAcctoThngListInfoServc'
+    const departname = 'rlDminsttNm=' +encodeURIComponent(req.params.departname)
+    console.log(departname)
+    const api = (url + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
+    const api1 = (url1 + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
+    const api2 = (url2 + "?"+ serviceKey + "&" + datasize + "&" + date + '&' + departname + '&' + type)
+    let dataSet = []
+
+    getData = async (api) => {
+        const array = api.split('||')       
+        await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
+        .then(axios.spread((result, result1, result2) => {
+            if (result.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result.data.response.body.items.length ; i++){
+                    dataSet.push(result.data.response.body.items[i])
+                }                
+            }
+            if (result1.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result1.data.response.body.items.length ; i++){
+                    dataSet.push(result1.data.response.body.items[i])
+                }                
+            }
+            if (result2.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result2.data.response.body.items.length ; i++){
+                    dataSet.push(result2.data.response.body.items[i])
+                }                
+            }
+            dataSet.sort((a, b) => {
+                return a.rcptDt > b.rcptDt ? -1 :  a.rcptDt < b.rcptDt ? 1 : 0
+            })
+            res.send(dataSet)
        }))
     }
     getData(api + '||' + api1 + '||' + api2)
@@ -92,27 +112,34 @@ app.get('/task/bone', (req, res) => {
     const api = (url + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
     const api1 = (url1 + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
     const api2 = (url2 + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
-
     let dataSet = []
+
     getData = async (api) => {
-        const array = api.split("||")
+        const array = api.split('||')       
         await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
-        .then(
-            axios.spread((result, result1, result2) => {
-                if (result.data.response.body.totalCount != 0) {
-                    dataSet.push(result.data.response.body)
-                }
-                if (result1.data.response.body.totalCount != 0) {
-                    dataSet.push(result1.data.response.body)
-                }
-                if (result2.data.response.body.totalCount != 0) {
-                    dataSet.push(result2.data.response.body)
-                }
-                res.json({'bone': dataSet})
+        .then(axios.spread((result, result1, result2) => {
+            if (result.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result.data.response.body.items.length ; i++){
+                    dataSet.push(result.data.response.body.items[i])
+                }                
+            }
+            if (result1.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result1.data.response.body.items.length ; i++){
+                    dataSet.push(result1.data.response.body.items[i])
+                }                
+            }
+            if (result2.data.response.body.totalCount != 0) {
+                for(let i = 0 ; i < result2.data.response.body.items.length ; i++){
+                    dataSet.push(result2.data.response.body.items[i])
+                }                
+            }
+            dataSet.sort((a, b) => {
+                return a.bidNtceDt > b.bidNtceDt ? -1 :  a.bidNtceDt < b.bidNtceDt ? 1 : 0
             })
-        )
+            res.send(dataSet)
+       }))
     }
-    getData(api + "||" + api1 + "||" + api2)
+    getData(api + '||' + api1 + '||' + api2)
 })
 
 app.get('/task/bone/:departname', (req, res) => {
@@ -131,47 +158,24 @@ app.get('/task/bone/:departname', (req, res) => {
         await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
         .then(axios.spread((result, result1, result2) => {
             if (result.data.response.body.totalCount != 0) {
-                dataSet.push(result.data.response.body)
+                for(let i = 0 ; i < result.data.response.body.items.length ; i++){
+                    dataSet.push(result.data.response.body.items[i])
+                }                
             }
             if (result1.data.response.body.totalCount != 0) {
-                dataSet.push(result1.data.response.body)
+                for(let i = 0 ; i < result1.data.response.body.items.length ; i++){
+                    dataSet.push(result1.data.response.body.items[i])
+                }                
             }
             if (result2.data.response.body.totalCount != 0) {
-                dataSet.push(result2.data.response.body)
+                for(let i = 0 ; i < result2.data.response.body.items.length ; i++){
+                    dataSet.push(result2.data.response.body.items[i])
+                }                
             }
-            console.log("dataSet", dataSet)
-            res.json({'bone' : dataSet})
-       }))
-    }
-    getData(api + '||' + api1 + '||' + api2)
-})
-
-app.get('/task/bone/', (req, res) => {
-    const url = 'http://apis.data.go.kr/1230000/BidPublicInfoService02/getBidPblancListInfoCnstwkPPSSrch'
-    const url1 = 'http://apis.data.go.kr/1230000/BidPublicInfoService02/getBidPblancListInfoServcPPSSrch'
-    const url2 = 'http://apis.data.go.kr/1230000/BidPublicInfoService02/getBidPblancListInfoThngPPSSrch'
-    const departname = 'dminsttNm=%EA%B5%AD%EB%AF%BC%EA%B1%B4%EA%B0%95%EB%B3%B4%ED%97%98%EA%B3%B5%EB%8B%A8'
-
-    const api = (url + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
-    const api1 = (url1 + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
-    const api2 = (url2 + "?"+ serviceKey + "&" + datasize + "&inqryDiv=1&" + date + '&' + departname + '&' + type)
-    let dataSet = []
-
-    getData = async (api) => {
-        const array = api.split('||')       
-        await axios.all([axios.get(array[0]), axios.get(array[1]), axios.get(array[2])])
-        .then(axios.spread((result, result1, result2) => {
-            if (result.data.response.body.totalCount != 0) {
-                dataSet.push(result.data.response.body)
-            }
-            if (result1.data.response.body.totalCount != 0) {
-                dataSet.push(result1.data.response.body)
-            }
-            if (result2.data.response.body.totalCount != 0) {
-                dataSet.push(result2.data.response.body)
-            }
-            console.log("dataSet", dataSet)
-            res.json({'bone' : dataSet})
+            dataSet.sort((a, b) => {
+                return a.bidNtceDt > b.bidNtceDt ? -1 :  a.bidNtceDt < b.bidNtceDt ? 1 : 0
+            })
+            res.send(dataSet)
        }))
     }
     getData(api + '||' + api1 + '||' + api2)
