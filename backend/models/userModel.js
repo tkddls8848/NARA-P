7e4f22ee6e4414a88e6c5b4332f53529cacbace6
PSI <tkddls8848@gmail.com>
2022-02-13 16:11:41 +0900

    [backend] user login CRUD

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose')
 const schema = mongoose.Schema
 
 const userSchema = new schema({
-    id: String,
-    pw: String
+    id: {
+        type: String,
+        unique: true
+    },
+    pw: String,
+    email: {
+        type: String,
+        required: false
+    }
 }, {versionKey: false})
 
 module.exports = mongoose.model('userlists', userSchema)
--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -9,14 +9,41 @@ mongoose.connect(url).then((result) => {
     console.log("LOGIN MONGO ERR", err)
 })
 
-router.post('/', (req, res) => {
-    console.log('LOGIN', req.body)
+router.post('/', async (req, res) => {
+    console.log('/', req.body, typeof(req.body.id))
+    const currentUser = await userModel.findOne({id: req.body.id}).exec()
+    if (currentUser == null) {
+        return res.json({id: 'TESTNULL', pw: 'TESTNULL', 'status': process.env.STATUS_NO_REGITERED})
+    } else {
+        const userId = currentUser.id
+        const userPw = currentUser.pw
+        return res.json({id: userId, pw: userPw, 'status': process.env.STATUS_REGITERED})
+    }
 })
 
 router.post('/join', async (req, res) => {
     console.log('JOIN', req.body)
-    const userJoin = new userModel({id: req.body.id, pw: req.body.password})
+    const userJoin = new userModel({id: req.body.id, pw: req.body.password, email: req.body.email})
     await userJoin.save()
+    return res.json({'status': process.env.STATUS_JOIN})
+})
+
+router.patch('/modify', async (req, res) => {
+    if(req.body.status == process.env.STATUS_NO_REGITERED) {
+        console.log('not register')
+        return res.status(404)
+    } else if(req.body.status == process.env.STATUS_REGITERED) {
+        console.log("MODIFY", req.body)
+        await userModel.updateOne({id: req.body.id}, {$set: {id: req.body.id, pw: req.body.pw, email: req.body.email}}).exec()
+        return res.json({'message':'user update complete'})
+    }
+})
+
+router.delete('/delete', async (req, res) => {
+    console.log('DELETE', req.body)
+    const uid = req.body.id
+    await userModel.deleteOne({'id': req.body.id})
+    return res.json({'message': uid + ' ' + 'delete comlpete'})
 })
 
 module.exports = router
