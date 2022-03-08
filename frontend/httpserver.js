c2fdc51d470b0eca72b2fd78148ed4225f694375
PSI <tkddls8848@gmail.com>
2022-03-08 21:24:53 +0900

    [backend] http and https server for dev

---
+++ frontend/httpserver.js
@@ -0,0 +1,52 @@
+const express = require("express")
+const next = require("next")
+const mongoose = require('mongoose')
+require('dotenv').config({ path: './.env.http.local' })
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const dev = process.env.NODE_ENV !== "production"
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const url = process.env.MONGO_URL
+
+const taskRouter = require('./backend/routes/taskRouter')
+const userTaskRouter = require('./backend/routes/userTaskRouter')
+const loginRouter = require('./backend/routes/loginRouter')
+const searchLogicRouter = require('./backend/routes/searchLogicRouter')
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.use(cookieParser())
+  server.use(express.json())
+  server.use(cors({
+      origin: true,
+      credentials: true
+  }))
+  server.use(express.urlencoded( {extended : true } ))  
+  server.use('/api/v1/task', taskRouter)
+  server.use('/api/v1/usertask', userTaskRouter)
+  server.use('/api/v1/login', loginRouter)
+  server.use('/api/v1/logic', searchLogicRouter)
+  
+  mongoose.connect(url).then((result) => {
+    console.log("LOGIN MONGO CONNECT")
+  }).catch((err) => {
+    console.log("LOGIN MONGO ERR")
+  })
+
+  server.get("*", (req, res) => {
+    return handle(req, res)
+  })
+
+  server.get("/test", (req, res) => {
+    console.log('TEST')
+    return app.render(req, res, "/test")
+  })
+
+  server.listen(process.env.PORT, (err) => {
+    if (err) throw err
+    console.log("listening to http server")
+  })
+
+})
